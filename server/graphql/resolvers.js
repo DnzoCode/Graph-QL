f30d63d88538584abe75e77387c38d360addbcf7
darocode <derodriguez074@misena.edu.co>
2023-07-13 23:56:05 -0500

    Funcion de registro y busqueda

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,8 +1,13 @@
 import Project from "../models/Project.js"
+import Task from "../models/Task.js"
 
 export const resolver = {
     Query: {
-        hello: () => "Hello World"
+        hello: () => "Hello World",
+        projects: async () => await Project.find(),
+        project: async (_, {_id}) => await Project.findById(_id),
+        task: async (_, {_id}) => await Task.findById(_id),
+        tasks: async () => await Task.find()
     },
     Mutation:{
         createProject: async (_, {name, description}) => {
@@ -12,6 +17,26 @@ export const resolver = {
             })
             const saveProject = await project.save()
             return saveProject
+        },
+        createTask: async (_, {title, projectId}) => {
+            const projectFound = await Project.findById(projectId)
+            if(!projectFound) throw new Error('Project not found')
+            const task = new Task({
+                title,
+                projectId
+            })
+            const saveTask = await task.save()
+            return saveTask
+        },
+        deleteProject: async (_, {_id})=> {
+            const deletedProject = await Project.findByIdAndDelete(_id)
+            if(!deletedProject) throw new Error('Project not found')
+            return deletedProject
+        },
+        deleteTask: async(_, {_id})=>{
+            const deletedTask = await Task.findByIdAndDelete(_id)
+            if(!deletedTask) throw new Error('Task not found')
+            return deletedTask
         }
     }
 }
--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -3,16 +3,31 @@ import {gql} from 'graphql-tag'
 export const typeDefs = gql`
     type Query {
         hello: String
+        projects: [Project]
+        project(_id: ID!): Project 
+        task(_id: ID!): Task 
+        tasks: [Task]
     }
-    
+
     type Mutation {
         createProject(name: String, description: String): Project
+        deleteProject(_id: ID!): Project
+        createTask(title: String, projectId: ID): Task
+        deleteTask(_id: ID!): Task
     }
 
     type Project{
         _id: ID
         name: String
         description: String
+        createdAt: String
+        updatedAt: String
+    }
+
+    type Task{
+        _id:ID,
+        title: String
+        projectId: ID
         createdAt: String
         updatedAt: String
     }
